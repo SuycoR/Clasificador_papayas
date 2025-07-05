bf4043f52c0d42bc4d70777a15c715d1c17d6dc4
SuycoR <pedro.suyco@unmsm.edu.pe>
2025-07-05 11:01:49 -0500

    creacion de la pagina

---
+++ aplicacionweb/components/HistorialProcesamiento.tsx
@@ -0,0 +1,8 @@
+export default function HistorialProcesamiento() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
+      <h4>Historial de procesamiento</h4>
+      <p>(Aquí puedes mostrar una tabla o lista en el futuro)</p>
+    </div>
+  );
+}
---
+++ aplicacionweb/components/ResumenDiario.tsx
@@ -0,0 +1,10 @@
+export default function ResumenDiario() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
+      <h4>Resumen diario</h4>
+      <p>Total de papayas procesadas: 120</p>
+      <p>Maduras: 50</p>
+      <p>Parcialmente maduras: 30</p>
+    </div>
+  );
+}
